832c72661d74b5a0ba6aa0b9cc5ea5429d3ac0dc
connork <connorkelly-95@hotmail.co.uk>
2023-07-28 16:06:56 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -1,34 +1,37 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-const answerDiv = document.getElementById('answer');
+import { useState } from "react";
 
 const chosenAlien = {
-  name: 'klargon',
-  colour: 'purple',
+  name: "klargon",
+  colour: "purple",
   horn_count: 2,
   eye_count: 3,
-  eye_colour: 'red',
+  eye_colour: "red",
   hasAntenna: true,
   isFriendly: false,
-  skinTexture: 'smooth',
+  skinTexture: "smooth",
 };
 
 const colourQuestion = [
-  { checkFor: 'blue', question: 'is your alien blue?' },
-  { checkFor: 'green', question: 'is your alien green?' },
-  { checkFor: 'purple', question: 'is your alien purple?' },
+  { checkFor: "blue", question: "is your alien blue?" },
+  { checkFor: "green", question: "is your alien green?" },
+  { checkFor: "purple", question: "is your alien purple?" },
 ];
 
 const skinQuestion = [
-  { checkFor: 'furry', question: 'does your alien have fur?' },
-  { checkFor: 'scaly', question: 'does your alien have scales?' },
-  { checkFor: 'smooth', question: 'is your alien smooth?' },
+  { checkFor: "furry", question: "does your alien have fur?" },
+  { checkFor: "scaly", question: "does your alien have scales?" },
+  { checkFor: "smooth", question: "is your alien smooth?" },
 ];
 
 export default function Home() {
   const [answer, setAnswer] = useState(null);
-  const [questionAsked, setQuestionAsked] = useState('');
+  const [questionAsked, setQuestionAsked] = useState({
+    checkFor: "",
+    question: "",
+  });
+  const [state, setState] = useState("");
 
   function questionChecker(alienProp, checkFor) {
     if (alienProp === checkFor) {
@@ -40,7 +43,15 @@ export default function Home() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    // questionChecker('colour', questionAsked);
+    // const checkFor = JSON.parse(questionAsked).checkFor;
+    questionChecker(chosenAlien.colour, questionAsked.checkFor);
+  }
+
+  function updateQuestion(value) {
+    setQuestionAsked((curr) => {
+      const newObject = JSON.parse(value);
+      return newObject;
+    });
   }
 
   return (
@@ -49,20 +60,23 @@ export default function Home() {
         <select
           id="question"
           name="question"
-          value={questionAsked}
+          value={questionAsked.question}
           onChange={(e) => {
-            console.log(questionAsked);
-            setQuestionAsked(e.target.value);
+            updateQuestion(e.target.value);
           }}
         >
           {colourQuestion.map((q) => {
-            return <option key={q.checkFor}>{q.question}</option>;
+            return (
+              <option key={q.checkFor} value={JSON.stringify(q)}>
+                {q.question}
+              </option>
+            );
           })}
         </select>
         <button id="submit">Submit Question</button>
       </form>
       <div id="answer">
-        <p>{answer}</p>
+        <p>{answer ? "Yes" : "No"}</p>
       </div>
     </main>
   );
