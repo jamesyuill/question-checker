1a29f326b8a802446fcd5edfb2212b1566d03b0a
James Yuill <jamesyuill@jamess-mbp.lan>
2023-07-31 19:48:07 +0100

    updates with addition of alienProp which is the alien attribute to search for

--- app/page.js
+++ app/page.js
@@ -14,26 +14,43 @@ const chosenAlien = {
 };
 
 const colourQuestion = [
-  { checkFor: 'blue', question: 'is your alien blue?' },
-  { checkFor: 'green', question: 'is your alien green?' },
-  { checkFor: 'purple', question: 'is your alien purple?' },
+  { alienProp: 'colour', checkFor: 'blue', question: 'is your alien blue?' },
+  { alienProp: 'colour', checkFor: 'green', question: 'is your alien green?' },
+  {
+    alienProp: 'colour',
+    checkFor: 'purple',
+    question: 'is your alien purple?',
+  },
 ];
 
 const skinQuestion = [
-  { checkFor: 'furry', question: 'does your alien have fur?' },
-  { checkFor: 'scaly', question: 'does your alien have scales?' },
-  { checkFor: 'smooth', question: 'is your alien smooth?' },
+  {
+    alienProp: 'skinTexture',
+    checkFor: 'furry',
+    question: 'does your alien have fur?',
+  },
+  {
+    alienProp: 'skinTexture',
+    checkFor: 'scaly',
+    question: 'does your alien have scales?',
+  },
+  {
+    alienProp: 'skinTexture',
+    checkFor: 'smooth',
+    question: 'is your alien smooth?',
+  },
 ];
 
 export default function Home() {
   const [answer, setAnswer] = useState(null);
   const [questionAsked, setQuestionAsked] = useState({
+    alienProp: '',
     checkFor: '',
     question: '',
   });
 
   function questionChecker(alienProp, checkFor) {
-    if (alienProp === checkFor) {
+    if (chosenAlien[alienProp] === checkFor) {
       setAnswer(true);
     } else {
       setAnswer(false);
@@ -42,18 +59,17 @@ export default function Home() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    // const checkFor = JSON.parse(questionAsked).checkFor;
-    questionChecker(chosenAlien.colour, questionAsked.checkFor);
+    questionChecker(questionAsked.alienProp, questionAsked.checkFor);
   }
   return (
     <main>
       <form onSubmit={(e) => handleSubmit(e)}>
         <select
           id="question"
-          name="question"
           onChange={(e) => {
             const parsed = JSON.parse(e.target.value);
             console.log(parsed);
+            questionAsked.alienProp = parsed.alienProp;
             questionAsked.checkFor = parsed.checkFor;
             questionAsked.question = parsed.question;
             setQuestionAsked({ ...questionAsked });
